cc243005bddebd4fb95da80704081e6d514eeefc
Dima <dimon.belenok@gmail.com>
2023-01-15 01:39:05 +0200

    Done swiper and some load animation

--- js/script.js
+++ js/script.js
@@ -1,8 +1,25 @@
-window.addEventListener('load',windowLoaded);
+
+
+window.addEventListener("load",  windowLoaded);
+
+
 
 
 function windowLoaded(){
-    if(document.querySelector('.product__slider')){
-        new Swiper
+    if(document.querySelector('.product__slider')) {
+        new Swiper('.product__slider',{
+            loop: true,
+            speed:1000,
+            parallax:true,
+            mousewheel: true,
+            grabCursor:true,
+            keyboard: true,
+            on: {
+                init: function() {
+                    document.documentElement.classList.add('loaded');
+                }
+            }
+        });
     }
 }
+
